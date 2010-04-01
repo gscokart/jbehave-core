3cd2f7f55323c18e9a91c0d4e6cd54a1a556f01e
Mauro Talevi <mauro.talevi@aquilonia.org>
2010-04-01 10:08:32 +0200

    Removed need for injected classloader in the i18n story.

--- trunk/core/examples/trader/src/main/java/org/jbehave/examples/trader/i18n/ItTraderStory.java
+++ trunk/core/examples/trader/src/main/java/org/jbehave/examples/trader/i18n/ItTraderStory.java
@@ -1,33 +1,27 @@
 package org.jbehave.examples.trader.i18n;
 
-import java.util.Locale;
-
+import org.jbehave.core.JUnitStory;
 import org.jbehave.core.MostUsefulStoryConfiguration;
 import org.jbehave.core.StoryConfiguration;
-import org.jbehave.core.JUnitStory;
-import org.jbehave.core.PropertyBasedStoryConfiguration;
-import org.jbehave.core.model.KeyWords;
 import org.jbehave.core.i18n.I18nKeyWords;
 import org.jbehave.core.i18n.StringEncoder;
+import org.jbehave.core.model.KeyWords;
 import org.jbehave.core.parser.ClasspathStoryDefiner;
 import org.jbehave.core.parser.PatternStoryParser;
-import org.jbehave.core.parser.StoryDefiner;
 import org.jbehave.core.parser.UnderscoredCamelCaseResolver;
 import org.jbehave.core.reporters.PrintStreamStoryReporter;
-import org.jbehave.core.reporters.StoryReporter;
 import org.jbehave.core.steps.StepsConfiguration;
 import org.jbehave.core.steps.StepsFactory;
+
+import java.util.Locale;
 
 public class ItTraderStory extends JUnitStory {
 
     public ItTraderStory() {
-        this(Thread.currentThread().getContextClassLoader());
-    }
-
-    public ItTraderStory(final ClassLoader classLoader) {
         StoryConfiguration storyConfiguration = new MostUsefulStoryConfiguration();
         // use Italian for keywords
-        KeyWords keywords = keywordsFor(new Locale("it"), classLoader);
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        KeyWords keywords = new I18nKeyWords(new Locale("it"), new StringEncoder(), "org/jbehave/examples/trader/i18n/keywords", classLoader);
         storyConfiguration.useKeywords(keywords);
         storyConfiguration.useStoryDefiner(new ClasspathStoryDefiner(
                 new UnderscoredCamelCaseResolver(".story"),
@@ -38,10 +32,6 @@ public class ItTraderStory extends JUnitStory {
         StepsConfiguration stepsConfiguration = new StepsConfiguration();
         stepsConfiguration.useKeyWords(keywords);
         addSteps(new StepsFactory(stepsConfiguration).createCandidateSteps(new ItTraderSteps()));
-    }
-
-    protected static KeyWords keywordsFor(Locale locale, ClassLoader classLoader) {
-        return new I18nKeyWords(locale, new StringEncoder(), "org/jbehave/examples/trader/i18n/keywords", classLoader);
     }
 
 }
