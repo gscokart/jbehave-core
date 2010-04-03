641982e9bf9529fe3815a4b09105575f09e61dce
Paul Hammant <paul@hammant.org>
2010-04-03 17:25:59 -0500

    unspecified delegation a little clearer now

--- trunk/core/jbehave-core/src/main/java/org/jbehave/core/RunnableStoryDelegator.java
+++ trunk/core/jbehave-core/src/main/java/org/jbehave/core/RunnableStoryDelegator.java
@@ -1,13 +1,14 @@
 package org.jbehave.core;
 
+import org.jbehave.core.errors.InvalidRunnableStoryException;
 import org.jbehave.core.steps.CandidateSteps;
 
 import java.util.List;
 
 /**
  * <p>
- * Delegates {@link RunnableStory} functionality to the delegate provided by
- * the concrete subclasses.  A default no-op implementation is given as default.
+ * Delegates {@link RunnableStory} functionality to a delegate specified in a
+ * post-instantiation call to delegateTo(..) 
  * </p>
  * <p>
  * Note that we cannot use a constructor as some delegates, e.g. {@JUnitStory} can
@@ -18,36 +19,42 @@ import java.util.List;
  */
 public abstract class RunnableStoryDelegator implements RunnableStory {
 
-    // Delegate must be provided by concrete subclasses
-    // No-op impl will fail with InvalidRunnableStoryException
-    private RunnableStory delegate = new AbstractStory(){};
+
+    private RunnableStory delegate;
 
     protected void delegateTo(RunnableStory delegate) {
         this.delegate = delegate;
     }
 
+    private RunnableStory getDelegate() {
+        if (delegate == null) {
+            throw new InvalidRunnableStoryException("Use delegateTo(..) to specify a delegate Runnable Story");
+        }
+        return delegate;
+    }
+
     public void runStory() throws Throwable {
-        this.delegate.runStory();
+        this.getDelegate().runStory();
     }
 
     public void useConfiguration(StoryConfiguration configuration) {
-        this.delegate.useConfiguration(configuration);
+        this.getDelegate().useConfiguration(configuration);
     }
 
     public StoryConfiguration getConfiguration() {
-        return delegate.getConfiguration();
+        return getDelegate().getConfiguration();
     }
 
     public void addSteps(CandidateSteps... steps) {
-        this.delegate.addSteps(steps);
+        this.getDelegate().addSteps(steps);
     }
 
     public List<CandidateSteps> getSteps() {
-        return delegate.getSteps();
+        return getDelegate().getSteps();
     }
 
     public void generateStepdoc() {
-        this.delegate.generateStepdoc();
+        this.getDelegate().generateStepdoc();
     }
 
 }
